5d164cb0764b3b8c4e00906109dae36a6b06ff2d
Wouter van Os <wouter0100@gmail.com>
2017-10-25 13:29:06 +0200

    Implement receiving of team icon

--- webview.js
+++ webview.js
@@ -1,3 +1,36 @@
+import { ipcRenderer } from 'electron';
+
+const getTeamIcon = function getTeamIcon() {
+  console.log('getTeamIcon');
+
+  let manifestElement = document.querySelector('link[rel="manifest"]');
+
+  if (manifestElement == null) {
+    return;
+  }
+
+  let manifestUrl = manifestElement.getAttribute('href');
+
+  if (manifestUrl == null) {
+    return;
+  }
+
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState != 4 || this.status != 200) {
+      return;
+    }
+
+    let response = JSON.parse(this.responseText);
+
+    if (response.icons.length >= 1) {
+      ipcRenderer.sendToHost('avatar', window.location.protocol + '//' + window.location.host + '/' + response.icons[0]['src']);
+    }
+  };
+  xmlhttp.open('GET', manifestUrl, true);
+  xmlhttp.send();
+};
+
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     const directMessages = Math.round(document.querySelectorAll('.unread.unread-mention').length / 2);
@@ -6,4 +39,8 @@ module.exports = (Franz) => {
   };
 
   Franz.loop(getMessages);
+
+  setTimeout(() => {
+    getTeamIcon();
+  }, 4000);
 };

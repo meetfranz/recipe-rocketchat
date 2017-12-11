792e6115596f9c51ab827f86f5f4e248d2f112de
Stefan Malzner <stefan@adlk.io>
2017-12-11 16:58:21 +0100

    Update message selector for 0.59.4

--- webview.js
+++ webview.js
@@ -3,28 +3,28 @@ const { ipcRenderer } = require('electron');
 const getTeamIcon = function getTeamIcon() {
   console.log('getTeamIcon');
 
-  let manifestElement = document.querySelector('link[rel="manifest"]');
+  const manifestElement = document.querySelector('link[rel="manifest"]');
 
   if (manifestElement == null) {
     return;
   }
 
-  let manifestUrl = manifestElement.getAttribute('href');
+  const manifestUrl = manifestElement.getAttribute('href');
 
   if (manifestUrl == null) {
     return;
   }
 
-  let xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
     if (this.readyState != 4 || this.status != 200) {
       return;
     }
 
-    let response = JSON.parse(this.responseText);
+    const response = JSON.parse(this.responseText);
 
     if (response.icons.length >= 1) {
-      ipcRenderer.sendToHost('avatar', window.location.protocol + '//' + window.location.host + '/' + response.icons[0]['src']);
+      ipcRenderer.sendToHost('avatar', `${window.location.protocol}//${window.location.host}/${response.icons[0].src}`);
     }
   };
   xmlhttp.open('GET', manifestUrl, true);
@@ -33,8 +33,8 @@ const getTeamIcon = function getTeamIcon() {
 
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-    const directMessages = Math.round(document.querySelectorAll('.unread.unread-mention').length / 2);
-    const indirectMessages = Math.round(document.querySelectorAll('.unread:not(.unread-mention)').length / 2);
+    const directMessages = Math.round(document.querySelectorAll('.unread.unread-mention, .badge--unread').length / 2);
+    const indirectMessages = Math.round(document.querySelectorAll('.unread:not(.unread-mention), .sidebar-item--unread').length / 2);
     Franz.setBadge(directMessages, indirectMessages);
   };
 

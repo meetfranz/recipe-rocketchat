e2bd5f6ded7d3584dba38fe0f54799b69fa7ab24
Felix Heilmeyer <code@fehe.eu>
2021-09-13 16:15:41 +0200

    Fix compatibility with RC 3.9.x and above

--- webview.js
+++ webview.js
@@ -34,9 +34,15 @@ const getTeamIcon = function getTeamIcon() {
 
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-    const directMessages = Math.round(document.querySelectorAll('.unread.unread-mention, .badge--unread').length / 2);
-    const indirectMessages = Math.round(document.querySelectorAll('.unread:not(.unread-mention), .sidebar-item--unread').length / 2);
-    Franz.setBadge(directMessages, indirectMessages);
+    if (document.querySelectorAll('.rc-box').length > 0) {
+      const directMessages = document.querySelectorAll('.rcx-badge--danger').length;
+      const indirectMessages = document.querySelectorAll(' .rcx-sidebar-item--highlighted:not(.rcx-badge--danger)').length;
+      Franz.setBadge(directMessages, indirectMessages);
+    } else {
+      const directMessages = Math.round(document.querySelectorAll('.unread.unread-mention, .badge--unread').length / 2);
+      const indirectMessages = Math.round(document.querySelectorAll('.unread:not(.unread-mention), .sidebar-item--unread').length / 2);
+      Franz.setBadge(directMessages, indirectMessages);
+    }
   };
 
   Franz.loop(getMessages);
